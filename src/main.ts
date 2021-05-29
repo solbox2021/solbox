423e4593d116d71a2788a90039e8e014def13586
Joe Wu <joe.wu@cootek.cn>
2021-05-29 18:02:46 +0800

    [feature] solve some eslint problems&  import auto layout and vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'windi.css'
+import './styles/main.css'
+import i18n from './i18n'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(i18n)
+  .use(router)
+  .mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import { PublicKey } from '@solana/web3.js'
+import { useI18n } from 'vue-i18n'
+
+const testKey = new PublicKey('DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1')
+
+const { t } = useI18n()
+
+</script>
+
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" />
+    <div class="text-red-500">
+      {{ testKey.toBase58() }}
+    </div>
+    <div>{{ t('button.toggle_dark') }}</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
