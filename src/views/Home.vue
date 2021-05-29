e98184a019ef1d9741d84fd94585d67ecb330184
Joe Wu <joe.wu@cootek.cn>
2021-05-29 12:34:47 +0800

    [feature] add some dependency, test solana sdk

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { PublicKey } from '@solana/web3.js'
+
+const testKey = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home flex flex-col items-center">
+    <img alt="Vue logo" src="../assets/logo.png" class="w-44 h-44" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ testKey.toString() }}</p>
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
-</script>
