e55fa696cbbccc74f7d7e3b21f691a063d418d43
Joe Wu <joe.wu@cootek.cn>
2021-05-29 20:18:15 +0800

    [feature] add Iconify support

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,10 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import { PublicKey } from '@solana/web3.js'
 import { useI18n } from 'vue-i18n'
+import { Icon, addCollection } from '@iconify/vue'
+import mdi from '@iconify/json/json/mdi.json'
+
+addCollection(mdi)
 
 const testKey = new PublicKey('DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1')
 
@@ -16,6 +20,7 @@ const { t } = useI18n()
       {{ testKey.toBase58() }}
     </div>
     <div>{{ t('button.toggle_dark') }}</div>
+    <Icon icon="mdi:account-voice" class="h-16 text-red-500 w-16" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
